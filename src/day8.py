31a62b8b839f467d54ceb88a89f65b2542222303
0x24d <daniel.longfield@tuta.io>
2021-01-04 15:41:29 +0000

    Add day 8 part 2 solution

--- src/day8.py
+++ src/day8.py
@@ -8,17 +8,60 @@ if __name__ == '__main__':
     ic = 0
     acc = 0
     used_instructions = []
-    while ic <= len(file_lines):
+    while ic < len(file_lines):
         if ic in used_instructions:
             break
         else:
             used_instructions.append(ic)
-        instructions = file_lines[ic].split(' ')
-        if instructions[0] == 'jmp':
-            ic += int(instructions[1])
+
+        instruction = file_lines[ic].split(' ')
+        if instruction[0] == 'jmp':
+            ic += int(instruction[1])
         else:
-            if instructions[0] == 'acc':
-                acc += int(instructions[1])
+            if instruction[0] == 'acc':
+                acc += int(instruction[1])
             ic += 1
     print(f'Accumulator value before second instruction run: {acc}')
+    
+    # Part 2
+    jmp_or_nop = True # jmp = True, nop = False
+    num_to_change = 0
+    num_jmps = len([j for j in file_lines if j.startswith('jmp')])
+    while True:
+        ic = 0
+        acc = 0
+        used_instructions = []
+        jmp_nop_visited = 0
+        while ic < len(file_lines):
+            if ic in used_instructions:
+                if jmp_or_nop and num_to_change == num_jmps:
+                    jmp_or_nop = False
+                    num_to_change = 0
+                else:
+                    num_to_change += 1
+                break
+            else:
+                used_instructions.append(ic)
+
+            instruction = file_lines[ic].split(' ')
+            if instruction[0] == 'jmp' or instruction[0] == 'nop':
+                if jmp_or_nop and instruction[0] == 'jmp':
+                    jmp_nop_visited += 1
+                    if jmp_nop_visited == num_to_change:
+                        instruction[0] = 'nop'
+                elif not jmp_nop_visited and instruction[0] == 'nop':
+                    jmp_nop_visited += 1
+                    if jmp_not_visited == num_to_change:
+                        instruction[0] = 'jmp'
+
+            if instruction[0] == 'jmp':
+                ic += int(instruction[1])
+            else:
+                if instruction[0] == 'acc':
+                    acc += int(instruction[1])
+                ic += 1
+        
+        if ic == len(file_lines):
+            break
+    print(f'Accumulator value after terminating program: {acc}')
     
